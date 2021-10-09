ed5e432429735f12fca687530ffd10c8c8d9e123
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-10-09 15:00:39 +0200

    Add First Test for GoogleTaskConfiguration

---
+++ bundles/org.openhab.binding.googletask/src/test/java/org/openhab/binding/googletask/GoogleTaskConfigurationTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package org.openhab.binding.googletask;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.openhab.binding.googletask.internal.GoogleTaskConfiguration;
+
+/**
+ * Testing the Google API Configuration
+ *
+ * @author konrad.eichstaedt@gmx.de on 09.10.21.
+ */
+public class GoogleTaskConfigurationTest {
+
+    @Test
+    void checkConfigurationValues() {
+        GoogleTaskConfiguration configuration = new GoogleTaskConfiguration();
+
+        assertNotNull(configuration);
+    }
+}
